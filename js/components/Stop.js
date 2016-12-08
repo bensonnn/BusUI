5a71c98400e11159503c0f697475cb11246316be
bensonnn <dan.s.benson@gmail.com>
2016-12-07 20:10:18 -0800

    css tweaks

--- js/components/Stop.js
+++ js/components/Stop.js
@@ -14,18 +14,18 @@ export default class Stop extends Component {
 
   minutesFromNow = (time) => {
     if (time === 'now') return time;
-    return moment(time).add(1, 'minute').diff(moment(), 'minutes');
+    return moment(time).add(1, 'minute').diff(moment(), 'minutes') + ' mins';
   }
 
   render() {
     const stop = this.props.data;
-    console.log(stop);
     return (
       <div className={ `stop color${this.props.colorId}` }>
-        <i className="fa fa-bus" aria-hidden="true"></i>
+      <i className="fa fa-bus" aria-hidden="true"></i>
         <div className='stop-name'>Stop { stop.stopId}</div>
         { stop.routes.map(route => {
-          return <div className='route'>Route { route.routeId } in { this.minutesFromNow(route.nextTimes[0]) } and { this.minutesFromNow(route.nextTimes[1]) } mins</div>
+          return <div className='route'>
+            Route { route.routeId } in <span className='highlight'>{ this.minutesFromNow(route.nextTimes[0]) }</span> and <span className='highlight'>{ this.minutesFromNow(route.nextTimes[1]) }</span></div>
         })}
 
       </div>
